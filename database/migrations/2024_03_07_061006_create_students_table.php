207085926d5d2a591b99d8cbc78e0e3da39c21e7
samndambuki <sam.ndambuki08@gmail.com>
2024-03-07 11:14:00 +0300

    created enrollments migration

---
+++ database/migrations/2024_03_07_061006_create_students_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            //id is the primary key which will auto increment
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('date_of_birth');
+            $table->string('gender');
+            $table->text('address');
+            $table->string('phone_number');
+            //created_at and updated_at columns to track when a record was last created
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
---
+++ database/migrations/2024_03_07_070134_create_enrollments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('enrollments', function (Blueprint $table) {
+            $table->id();
+            //foreignId - defines a foreign key column named stdent_id references id column in the students table 
+            //constrained - establishes foreign key constraints
+            //onDelete('cascade') - ensures that if a columnn in the students is deleted the  corresponding column in the enrollments is deleted
+            $table->foreignId('student_id')->constrained('students')->onDelete('cascade');
+            $table->foreignId('class_id')->constrained('classes')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('enrollments');
+    }
+};
