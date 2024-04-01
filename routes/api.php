ee6a9af845e2cebba01bc14a6654831617b02e4b
samndambuki <sam.ndambuki08@gmail.com>
2024-04-01 13:40:53 +0300

    tested create students endpoint

--- routes/api.php
+++ routes/api.php
@@ -16,37 +16,35 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-//ensures only authenticated users can access these routes
-Route::middleware('auth:sanctum')->group(function () {
-    /***STUDENT ROUTES***/
 
-    //fetches all students
-    Route::get('/students', [StudentController::class, 'index']);
-    //creates a new student 
-    Route::post('/students', [StudentController::class, 'store']);
-    //fetch a single student
-    Route::get('/students/{id}', [StudentController::class, 'show']);
-    //update a student
-    Route::put('/students/{id}', [StudentController::class, 'update']);
-    //delete a student
-    Route::delete('/students/{id}', [StudentController::class, 'destroy']);
+/***STUDENT ROUTES***/
 
-
-    /*** TEACHER ROUTES***/
-
-    //fetches all teachers
-    Route::get('/teachers', [TeacherContoller::class, 'index']);
-    //creates a new teacher
-    Route::post('/teachers', [TeacherContoller::class, 'store']);
-    //fetch a single teacher 
-    Route::get('/teachers/{id}', [TeacherContoller::class, 'show']);
-    //update a teacher 
-    Route::put('teachers/{id}', [TeacherContoller::class, 'update']);
-    //delete a teacher
-    Route::delete('teachers/{id}', [TeacherContoller::class, 'destroy']);
+//fetches all students
+Route::get('/students', [StudentController::class, 'index']);
+//creates a new student 
+Route::post('/students', [StudentController::class, 'store']);
+//fetch a single student
+Route::get('/students/{id}', [StudentController::class, 'show']);
+//update a student
+Route::put('/students/{id}', [StudentController::class, 'update']);
+//delete a student
+Route::delete('/students/{id}', [StudentController::class, 'destroy']);
 
 
-});
+/*** TEACHER ROUTES***/
+
+//fetches all teachers
+Route::get('/teachers', [TeacherContoller::class, 'index']);
+//creates a new teacher
+Route::post('/teachers', [TeacherContoller::class, 'store']);
+//fetch a single teacher 
+Route::get('/teachers/{id}', [TeacherContoller::class, 'show']);
+//update a teacher 
+Route::put('teachers/{id}', [TeacherContoller::class, 'update']);
+//delete a teacher
+Route::delete('teachers/{id}', [TeacherContoller::class, 'destroy']);
+
+
 
 
 
