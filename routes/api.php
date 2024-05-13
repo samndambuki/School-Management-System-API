6c3ae452b116668e75459f782d428514f56db54d
samndambuki <sam.ndambuki08@gmail.com>
2024-05-13 10:04:47 +0300

    successfully connected with frontend

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,14 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 
 /*** STUDENT ROUTES ***/
@@ -90,7 +94,3 @@ Route::post('/attendances', [AttendanceController::class, 'store']);
 Route::get('/attendances/{id}', [AttendanceController::class, 'show']);
 Route::put('/attendances/{id}', [AttendanceController::class, 'update']);
 Route::delete('/attendances/{id}', [AttendanceController::class, 'destroy']);
-
-
-
-
