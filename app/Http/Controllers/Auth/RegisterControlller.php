3a26bb09d50b4c767c8dc347eb7d808cfb0e386a
samndambuki <sam.ndambuki08@gmail.com>
2024-06-24 20:24:38 +0300

    worked on register controller

---
+++ app/Http/Controllers/Auth/RegisterControlller.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterControlller extends Controller
+{
+    //$request - instance of request which contains incoming request data
+    public function register(Request $request)
+    {
+        //input validation
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed'
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+        return response()->json(['message' => 'User registered successfully'], 201);
+    }
+}
